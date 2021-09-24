7eb7be8891d55dde612775449809c1616dfa8f91
Artem Kuptsov <kuptsov210@gmail.com>
2021-09-24 18:56:14 +0300

    Create base figure

---
+++ app/Models/Figure.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+abstract class Figure
+{
+    private $x;
+    private $y;
+
+    /**
+     * @return mixed
+     */
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * @param mixed $x
+     */
+    public function setX($x): void
+    {
+        $this->x = $x;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * @param mixed $y
+     */
+    public function setY($y): void
+    {
+        $this->y = $y;
+    }
+
+
+    protected function draw(): void
+    {
+        //TODO: Write basic draw method
+    }
+
+}

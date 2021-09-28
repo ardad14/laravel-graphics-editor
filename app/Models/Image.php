28e1cfffc2b86be6900a8c1237760b9b0dfe1a25
Artem Kuptsov <kuptsov210@gmail.com>
2021-09-28 15:20:02 +0300

    Create image model with picture params

---
+++ app/Models/Image.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    private static $imageFormat = "png";
+    private static $imageWidth = 1080;
+    private static $imageHeight = 720;
+    private array $imageFigures;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->imageFigures = array();
+    }
+
+    /**
+     * @return string
+     */
+    public static function getImageFormat(): string
+    {
+        return self::$imageFormat;
+    }
+
+    /**
+     * @param string $imageFormat
+     */
+    public static function setImageFormat(string $imageFormat): void
+    {
+        self::$imageFormat = $imageFormat;
+    }
+
+    /**
+     * @return int
+     */
+    public static function getImageWidth(): int
+    {
+        return self::$imageWidth;
+    }
+
+    /**
+     * @param int $imageWidth
+     */
+    public static function setImageWidth(int $imageWidth): void
+    {
+        self::$imageWidth = $imageWidth;
+    }
+
+    /**
+     * @return int
+     */
+    public static function getImageHeight(): int
+    {
+        return self::$imageHeight;
+    }
+
+    /**
+     * @param int $imageHeight
+     */
+    public static function setImageHeight(int $imageHeight): void
+    {
+        self::$imageHeight = $imageHeight;
+    }
+
+    /**
+     * @return array
+     */
+    public function getImageFigures(): array
+    {
+        return $this->imageFigures;
+    }
+
+    /**
+     * @param array $imageFigures
+     */
+    public function setImageFigures(array $imageFigures): void
+    {
+        $this->imageFigures = $imageFigures;
+    }
+
+
+
+
+}

a9d03d1b0e068820a8a3bf375bc057d2dcbb84a9
Artem Kuptsov <kuptsov210@gmail.com>
2021-09-29 13:37:58 +0300

    Edit drawing method for square

--- app/Models/Square.php
+++ app/Models/Square.php
@@ -4,29 +4,25 @@ namespace App\Models;
 
 class Square extends Rectangle
 {
-    private int $length;
-
     /**
      * @param int $x1
      * @param int $y1
-     * @param int $x2
-     * @param int $y2
-     * @param int $height
+     * @param int $length
      */
     public function __construct(int $x1, int $y1, int $length)
     {
-        $this->setX($x1);
-        $this->setY($y1);
-        $this->length = $length;
+        parent::__construct(
+            $x1,
+            $y1,
+            $x1 + sqrt(pow($length, 2) * 2),
+            $y1 + sqrt(pow($length, 2) * 2)
+        );
+
     }
 
-
-    public function draw(int $colorCode, array $points): array
+    public function draw(int $colorCode): array
     {
-        $points[2] = $points[0] + sqrt(pow($this->length, 2) * 2);
-        $points[3] = $points[1] + sqrt(pow($this->length, 2) * 2);
-        $parentResult = parent::draw($colorCode, $points);
-
+        $parentResult = parent::draw($colorCode);
         return array();
     }
 }

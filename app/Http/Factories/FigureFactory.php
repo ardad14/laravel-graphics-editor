24b3a9811aa4f60f1fa08b10fac5d4ccfcee4615
Artem Kuptsov <kuptsov210@gmail.com>
2021-09-30 10:55:10 +0300

    Edit figure factory to make new figure

--- app/Http/Factories/FigureFactory.php
+++ app/Http/Factories/FigureFactory.php
@@ -3,10 +3,14 @@
 namespace App\Http\Factories;
 
 use App\Models\Circle;
+use App\Models\Ellipse;
+use App\Models\Parallelogram;
 use App\Models\Point;
 use App\Models\Rectangle;
 use App\Models\Section;
 use App\Models\Square;
+use App\Models\Text;
+use App\Models\Triangle;
 
 class FigureFactory
 {
@@ -30,8 +34,28 @@ class FigureFactory
         return new Rectangle($x1, $y1, $x2, $y2);
     }
 
+    public static function makeParallelogram($x1, $y1, $x2, $y2, $x3, $y3): Parallelogram
+    {
+        return new Parallelogram($x1, $y1, $x2, $y2, $x3, $y3);
+    }
+
     public static function makeCircle($x1, $y1, $radius): Circle
     {
         return new Circle($x1, $y1, $radius);
+    }
+
+    public static function makeEllipse($x1, $y1, $longRadius, $shortRadius): Ellipse
+    {
+        return new Ellipse($x1, $y1, $longRadius, $shortRadius);
+    }
+
+    public static function makeTriangle($x1, $y1, $x2, $y2, $x3, $y3): Triangle
+    {
+        return new Triangle($x1, $y1, $x2, $y2, $x3, $y3);
+    }
+
+    public static function makeText($x, $y, $text, $fontSize): Text
+    {
+        return new Text($x, $y, $text, $fontSize);
     }
 }

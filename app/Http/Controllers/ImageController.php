9fb61dbd7412f512a6c713b4e8d57b20a53cf62d
Artem Kuptsov <kuptsov210@gmail.com>
2021-09-30 10:55:54 +0300

    Refactor image controller with unused connections

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,16 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PointRequest;
-use App\Http\Requests\RectangleRequest;
 use App\Http\Services\ImageService;
-use App\Models\Point;
-use App\Http\Requests\SectionRequest;
-use App\Models\Rectangle;
-use App\Models\Section;
-use App\Http\Requests\SquareRequest;
-use App\Models\Square;
-use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {

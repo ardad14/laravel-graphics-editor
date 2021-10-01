d6199d08663c2e532735eafe5821be407bce3853
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-01 18:44:29 +0300

    Edit image controller to JavaScripts requests

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\FileService;
 use App\Http\Services\ImageService;
+use Faker\Core\File;
 
 class ImageController extends Controller
 {
     public function draw()
     {
         ImageService::drawFigure($_REQUEST);
+    }
+
+    public function getFigure()
+    {
+        FileService::sendFigure();
+    }
+
+    public function clearCanvas()
+    {
+        FileService::clearCanvas();
     }
 }

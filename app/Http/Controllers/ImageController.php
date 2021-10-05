e043104d277cf305d8ee33472cd720b4e6118a6d
Artem Kuptsov <kuptsov210@gmail.com>
2021-10-05 16:10:33 +0300

    Edit image controller to work with uploading files

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,30 +3,62 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SaveImageRequest;
-use App\Http\Requests\SquareRequest;
 use App\Http\Services\FileService;
 use App\Http\Services\ImageService;
 use App\Http\Requests\FigureRequest;
+use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
+    /**
+     * Get FigureRequest and draw all of figures
+     * @param FigureRequest $request
+     */
     public function draw(FigureRequest $request): void
     {
         ImageService::drawFigure($request->all());
     }
 
+    /**
+     * Send temp file temp_image.png to JavaScript
+     */
     public function getFigure(): void
     {
         FileService::sendFigure();
     }
 
+    /**
+     * Clear canvas through deleting temp file temp_image.png
+     */
     public function clearCanvas(): void
     {
         FileService::clearCanvas();
     }
 
+    /**
+     * Save image with user in DB
+     * @param SaveImageRequest $request
+     */
     public function saveImage(SaveImageRequest $request): void
     {
         FileService::saveImage($request);
+    }
+
+    /**
+     * Upload user`s custom picture at the canvas
+     * @param Request $request
+     */
+    public function uploadImage(Request $request): void
+    {
+        FileService::uploadImage($request);
+    }
+
+    /**
+     * Open old user images using DB and file system and make temp file temp_image.png
+     * @param $userId
+     */
+    public function openUserImage($userId): void
+    {
+        FileService::openUserImage($userId);
     }
 }
